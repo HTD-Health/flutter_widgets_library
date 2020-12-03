bd9b620deacf7368e281716f30d5b9562eca2d3d
Kamil Klyta <kamil.klyta@htdevelopers.com>
2020-12-03 12:02:12 +0100

    Add template/example

---
+++ packages/template/lib/my_flutter_widget.dart
@@ -0,0 +1,4 @@
+library my_flutter_widget;
+
+// this file will be exposed by this package
+export 'src/my_flutter_widget.dart';
---
+++ packages/template/lib/src/my_flutter_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/// Widget implementation
+class MyFlutterWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello world!");
+  }
+}
