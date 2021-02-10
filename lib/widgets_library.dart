70ecf309ae6a7a504b5aa0ad5b674745b73081b7
Kamil Klyta <kamil.klyta@htdevelopers.com>
2021-02-10 11:25:25 +0100

    Add slides dots widget

--- lib/widgets_library.dart
+++ lib/widgets_library.dart
@@ -11,3 +11,6 @@ export 'src/buttons/fade_button.dart' show FadeButton;
 /// Layouts
 export 'src/layouts/fill_viewport_sliver_list/fill_viewport_sliver_list.dart'
     show FillViewportSliverList;
+
+/// Slides
+export 'src/slides/slides_dots.dart' show SlidesDots;
